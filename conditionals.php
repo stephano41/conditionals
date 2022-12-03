f09acf588ead248622639d3aa98012531d3c4d34
steven <stevenzhangshao@gmail.com>
2022-12-03 15:32:28 +1030

    first commit

---
+++ conditionals.php
@@ -0,0 +1,27 @@
+<?php
+ 
+/**
+ 
+ * @package conditionals
+ 
+ */
+ 
+/*
+ 
+Plugin Name: conditionals
+ 
+Plugin URI: 
+ 
+Description: for custom conditional DKI
+ 
+Version: 0.0.1
+ 
+Author: Steven Zhang
+ 
+Author URI: 
+ 
+License: GPLv2 or later
+ 
+Text Domain: conditionals
+ 
+*/
