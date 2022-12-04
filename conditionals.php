40e4860baa1ecf6b1667fb6cf6532d2c4a704406
Steven <stevenzhangshao@gmail.com>
2022-12-04 20:06:12 +1030

    draft of utilising meta

--- conditionals.php
+++ conditionals.php
@@ -24,18 +24,22 @@ License: GPLv2 or later
  
 Text Domain: conditionals
  
-*/function wporg_shortcode( $atts = [], $content = null) {
+*/
+function timetable_shortcode( $atts = [], $content = null) {
     // do something to $content
     // always return
     // check if logged in
     if (get_current_user_id()) { 
-
-        return get_user_meta(get_current_user_id(), 'Stream', True);
-    } else {
-        return "You are not logged in!";
+        switch (get_user_meta(get_current_user_id(), 'Stream', True)){
+            case 'A':
+                return "<a href='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mommyismyteacher.com%2Fletter-a-printables&psig=AOvVaw2KsEkmfPPFhrx7xBhHrgCt&ust=1670231884155000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJjBnOPQ3_sCFQAAAAAdAAAAABAE'>you're A</a>";
+            case 'B':
+                return "<img src='" . plugin_dir_url( __FILE__ ) ."timetables/640px-LetterB.png'>";
+        }
     }
+    return 0;
 }
     
     
 
-add_shortcode('wporg', 'wporg_shortcode');
+add_shortcode('timetable', 'timetable_shortcode');
