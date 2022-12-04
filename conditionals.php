4982ab8f4d766e3fe16e49811b54371ca3399932
steven <stevenzhangshao@gmail.com>
2022-12-04 18:31:23 +1030

    reading metadata

--- conditionals.php
+++ conditionals.php
@@ -24,4 +24,18 @@ License: GPLv2 or later
  
 Text Domain: conditionals
  
-*/
+*/function wporg_shortcode( $atts = [], $content = null) {
+    // do something to $content
+    // always return
+    // check if logged in
+    if (get_current_user_id()) { 
+
+        return get_user_meta(get_current_user_id(), 'Stream', True);
+    } else {
+        return "You are not logged in!";
+    }
+}
+    
+    
+
+add_shortcode('wporg', 'wporg_shortcode');
