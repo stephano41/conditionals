8ce8d1a074448a3b73863a8a4c708ab9a5dc5075
Steven <stevenzhangshao@gmail.com>
2023-01-13 17:52:30 +1030

    which stream shortcode

--- conditionals.php
+++ conditionals.php
@@ -34,7 +34,7 @@ define("ONLINE_TICKET_NAME", "Online Conference Ticket");
     
 function generic_timetable_shortcode( $atts = [], $content = null) {
     if (!get_current_user_id()) {
-        return 0;
+        return null;
     }
 
     if (get_user_online(ONLINE_TICKET_NAME)){
@@ -52,7 +52,7 @@ function is_online_shortcode($atts = [], $content = null) {
     ), $atts );
 
     if (!get_current_user_id()) {
-        return 0;
+        return null;
     }
 
     if (get_user_online(ONLINE_TICKET_NAME)){
@@ -63,5 +63,28 @@ function is_online_shortcode($atts = [], $content = null) {
 
 }
 
+function which_stream_shortcode($atts = [], $content = null) {
+    $a = shortcode_atts( array(
+        'is_a' => null,
+        'is_b' => null,
+    ), $atts );
+
+    if (!get_current_user_id()) {
+        return null;
+    }
+
+    switch(get_user_stream()){
+        case "A":
+            // return implode(array_keys($a));
+            return $a['is_a'];
+        case "B":
+            return $a['is_b'];
+        default:
+            return null;
+    }
+
+}
+
+add_shortcode('which_stream', "which_stream_shortcode");
 add_shortcode('generic_timetable', 'generic_timetable_shortcode');
 add_shortcode('is_online', 'is_online_shortcode');
--- utils.php
+++ utils.php
@@ -3,3 +3,7 @@
 function get_user_online($ticket_name){
     return get_user_meta(get_current_user_id(), 'Ticket Type', True) == $ticket_name;
 }
+
+function get_user_stream(){
+    return get_user_meta(get_current_user_id(), 'Stream preference', True);
+}
